b9ba2ed67cffdc090c1da581435b1b9d4c9748ae
Marc Becklein <marc.becklein@gmx.de>
2022-04-06 22:00:10 +0200

    Added UE5 Project

---
+++ Source/CurseOfImmortality/CurseOfImmortality.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CurseOfImmortality.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CurseOfImmortality, "CurseOfImmortality" );
