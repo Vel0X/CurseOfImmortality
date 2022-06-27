34830cf493347c76cfcd0c8174578a43603ef7f8
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2022-06-27 11:38:52 +0200

    WIP Moloch

---
+++ Source/CurseOfImmortality/AI/Moloch/States/MolochPrepareCharge.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CurseOfImmortality/AI/Moloch/States/MolochBaseState.h"
+#include "MolochPrepareCharge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CURSEOFIMMORTALITY_API UMolochPrepareCharge : public UMolochBaseState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CurseOfImmortality/AI/Moloch/States/MolochStomping.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CurseOfImmortality/AI/Moloch/States/MolochBaseState.h"
+#include "MolochStomping.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CURSEOFIMMORTALITY_API UMolochStomping : public UMolochBaseState
+{
+	GENERATED_BODY()
+	
+};
